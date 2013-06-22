d9803dee6ce7b9087fa17d20a99fad286fa0d45d
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-06-22 13:27:23 +0200

    Added error handling to plugin

--- plugin/popup.js
+++ plugin/popup.js
@@ -5,7 +5,42 @@ if (typeof String.prototype.startsWith != 'function') {
 	};
 }
 
+// checks for time format
+function correctTimeFormat(t) {
+	p = t.split(":");
+
+	// correct format
+	if(p.length != 3)
+		return false;
+
+	// only two digits
+	for(var i in p)
+		if(p[i].toString().length != 2)
+			return false;
+
+	// correct range
+	var h = parseInt(p[0]);
+	var m = parseInt(p[1]);
+	var s = parseInt(p[2]);
+	if(isNaN(h) || h < 0 || h > 24)
+		return false;
+	if(isNaN(m) || m < 0 || m >= 60)
+		return false;
+	if(isNaN(s) || s < 0 || s >= 60)
+		return false
+
+	return true;
+}
+
 window.onload = function() {
+	// check time format
+	document.getElementById("time").addEventListener("keyup", function() {
+		if(correctTimeFormat(document.getElementById("time").value))
+			document.getElementById("time").className = "right";
+		else
+			document.getElementById("time").className = "wrong";
+	});
+
 	// set onClick handler
 	document.getElementById("submit").onclick = function() {
 		chrome.tabs.getSelected(function(tab) {
