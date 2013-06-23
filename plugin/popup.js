fd5ef4e17053e6b4fc92754c79b73265d74f235c
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-06-23 20:36:42 +0200

    lots of improvements

--- plugin/popup.js
+++ plugin/popup.js
@@ -9,25 +9,61 @@ window.onload = function() {
 	// set up text fields
 	fields = ["h", "m", "s"];
 	var then = new Date(new Date().getTime() + 30 * 1000);
+
+	// handle key press
+	var keydown = 0;
+	var pressedKeyCodes = [];
+
 	for(var i in fields) {
 		document.getElementById("time_" + fields[i]).type = "text";
 		document.getElementById("time_" + fields[i]).className = "right";
 		document.getElementById("time_" + fields[i]).size = 2;
-		document.getElementById("time_" + fields[i]).maxlength = 2;
+		document.getElementById("time_" + fields[i]).maxLength = 2;
 		
-		document.getElementById("time_" + fields[i]).addEventListener("keyup", function() {
+		document.getElementById("time_" + fields[i]).addEventListener("keyup", function(e) {
+			if(pressedKeyCodes.length <= 1) {
+				var val = this.value;
+				if(val.length == 2) {
+					var id = this.id;
+					if(id == "time_h") {
+						document.getElementById("time_m").focus();
+						document.getElementById("time_m").select();
+					} else if(id == "time_m") {
+						document.getElementById("time_s").focus();
+						document.getElementById("time_s").select();
+					} else if(id == "time_s")
+						document.getElementById("submit").focus();
+				}
+			}
+			var ind = pressedKeyCodes.indexOf(e.keyCode);
+			pressedKeyCodes.splice(ind, 1);
+
+			// error checking
 			var val = this.value;
-			if(val.length == 2) {
+			if(isNaN(val))
+				this.className = "wrong";
+			else {
+				this.className = "right";
+
 				var id = this.id;
 				if(id == "time_h") {
-					document.getElementById("time_m").focus();
-					document.getElementById("time_m").select();
-				} else if(id == "time_m") {
-					document.getElementById("time_s").focus();
-					document.getElementById("time_s").select();
-				} else if(id == "time_s")
-					document.getElementById("submit").focus();
-			}
+					if(val < 0 || val > 24)
+						this.className = "wrong";
+				} else {
+					if(val < 0 || val > 59)
+						this.className = "wrong";
+				}
+
+				if(val.toString().length != 2)
+					this.className = "wrong";
+			}	
+		});
+		
+		document.getElementById("time_" + fields[i]).addEventListener("keydown", function(e) {
+			var kc = e.keyCode;
+			if(pressedKeyCodes.indexOf(kc) != -1) 
+				return;
+			pressedKeyCodes.push(kc);
 		});
 
 		if(fields[i] == "h")
